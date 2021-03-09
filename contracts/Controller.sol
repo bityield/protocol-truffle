e4583a18c412a0f60cfeded3125ca4ed5b30a573
Alex Manelis <amanelis@gmail.com>
2021-03-09 10:45:27 -0800

    Updated controller instance cars for custodians

--- contracts/Controller.sol
+++ contracts/Controller.sol
@@ -7,7 +7,25 @@ import { AddressArrayUtils } from "./lib/AddressArrayUtils.sol";
 contract Controller is Ownable {
 	using AddressArrayUtils for address[];
 	
-	constructor() public {
+	address public dispatcher;
+	address public deployer;
+	address public controller;
+	address public supervisor;
+	address public custodian;
+	
+	constructor(
+		address _dispatcher,
+		address _deployer,
+		address _controller,
+		address _supervisor,
+		address _custodian
+	) public {
 		Ownable(msg.sender);
+		
+		dispatcher = _dispatcher;
+		deployer = _deployer;
+		controller = _controller;
+		supervisor = _supervisor;
+		custodian = _custodian;
 	}
 }
